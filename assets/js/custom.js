3f7624b9a33197e6a4325825f4832ba320b3e669
zahidrmpi <zahidhassan19@gmail.com>
2020-12-17 22:40:12 +0600

    static 1st demo page upload

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            loop:true
+        },
+        600:{
+            items:1,
+            nav:false,
+            loop:true
+        },
+        1000:{
+            items:1,
+            nav:false,
+            loop:true
+        }
+    }
+})
